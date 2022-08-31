2f4099083b8c707d9f91a4a3a9e2e291cde2d26c
Pseudo <v.pseudo.11@gmail.com>
2022-08-31 10:54:55 -0500

    Home: design completion

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import styled from 'styled-components'
+import Home from './components/Home'
+import Pokedex from './components/Pokedex'
+import PokemonDetails from './components/PokemonDetails'
+
+import ProtectedRoutes from './components/ProtectedRoutes'
+
+function App() {
+
+  return (
+    <AppContainer>
+      <Routes>
+        <Route path='/' element={<Home />} />
+
+        <Route element={<ProtectedRoutes />}>
+          <Route path="/pokedex" element={<Pokedex />} />
+          <Route path="/pokedex/:id" element={<PokemonDetails />} />
+        </Route>
+      </Routes>
+    </AppContainer>
+  )
+}
+
+const AppContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  background-color: #F8F8F8;
+`
+
+export default App
---
+++ src/components/Home.jsx
@@ -0,0 +1,149 @@
+import React from 'react'
+import styled from 'styled-components'
+
+import PikachuGif from './../assets/Pikachu.gif'
+import Pokeball from './../assets/Pokeball (1).gif'
+
+const Home = () => {
+    const clickbutton = () => {
+        console.log('click')
+    }
+    return (
+        <HomeContainer>
+            <h1><span>Poké</span>dex</h1>
+            <FirstSection>
+                <h1>Hello Trainer!!!</h1>
+                <img src={PikachuGif} alt="" />
+            </FirstSection>
+            <SecondSection>
+                <h2>Give me your name to start</h2>
+                <DivInput>
+                    <InputContainer type="text" />
+                    <ButtonContainer onClick={clickbutton}>
+                        <img src={Pokeball} alt="" />
+                        <h2><span>G</span>o</h2>
+                    </ButtonContainer>
+                </DivInput>
+            </SecondSection>
+        </HomeContainer>
+    )
+}
+
+const HomeContainer = styled.div`
+    max-width: 1200px;
+    width: 100%;
+    height: 100vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    background-color: #F8F8F8;
+    &>h1{
+        font-size: 4rem;
+        color: #DFE1E1;
+        margin-bottom: 60px;
+        text-shadow: 3px 3px 3px rgba(0, 0, 0, .3);
+        border: 3px solid black;
+        border-radius: 30px;
+        background-color: #B5331C;
+        text-align: center;
+        &>span{
+            color: #B5331C;
+            background-color: #DFE1E1;
+            border-radius: 27px;
+        }
+        @media (max-width: 768px){
+        font-size: 3.3rem;
+    }
+    }
+`
+
+const FirstSection = styled.section`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    &>img{
+        width: 300px;
+    }
+
+    &>h1{
+        font-size: 1.8rem;
+    }
+    @media (max-width: 768px){
+        flex-direction: column;
+        &>h1{
+            font-size: 1.5rem;
+        }
+        &>img{
+            width: 150px;
+        }
+    }
+`
+
+const SecondSection = styled.section`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    margin: 60px 0;
+    @media (max-width: 768px){
+        font-size: .6rem;
+    }
+`
+
+const DivInput = styled.div`
+    margin-top: 15px;
+    display: flex;
+    align-items: center;
+`
+
+const InputContainer = styled.input`
+    background: rgba(0, 0, 0, .1);
+    border: 1px solid white;
+    border-radius: 15px;
+    width: 215px;
+    height: 30px;
+    text-align: center;
+    box-shadow: 3px 3px 5px rgba(0, 0, 0, .1);
+`
+
+const ButtonContainer = styled.button`
+    position: relative;
+    width: 45px;
+    height: 45px;
+    background: transparent;
+    border: none;
+    &>img{
+        width: 40px;
+        position: absolute;
+        top: 0;
+        left: 0;
+    }
+    &:hover img{
+        opacity: 0;
+        transition: all .3s ease;
+    }
+    &>h2{
+        position: absolute;
+        top: 0;
+        left: 0;
+        border: 1px solid red;
+        width: 40px;
+        height: 40px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        font-size: 1.5rem;
+        border-radius: 50%;
+        opacity: 0;
+        &>span{
+            color: red;
+        }
+    }
+    &:hover h2{
+        opacity: 1;
+        transition: all .3s ease;
+    }
+`
+export default Home
